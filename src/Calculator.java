0d9542771260f912306eed6dc0d09077f719784a
enzonguyen <188813593+TanDuong1332006@users.noreply.github.com>
2026-04-20 20:21:21 +0700

    Khoi tao project

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator app = new Calculator();
+        // Sau này bạn sẽ gọi các hàm ở đây để trình bày với thầy
+        System.out.println("--- CHƯƠNG TRÌNH MÁY TÍNH NHÓM ---");
+    }
+    // Các thành viên sẽ viết hàm cộng, trừ, nhân, chia, pt bậc nhất ở dưới này
+}
